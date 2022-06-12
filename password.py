e55ef770496ebf39c29f10f6356e66dff190579d
Olusoji Daniel <danielolusoji@gmail.com>
2022-06-11 18:20:06 -0700

    password checker

---
+++ password.py
@@ -0,0 +1,9 @@
+print ("Welcome to password checker")
+
+username = input("Enter Username")
+password = input("Enter password")
+password2 = input("Re-Enter password")
+while password2 != password:
+    password2 = input("Re-Enter password")
+    if password2 == password:
+        print("You're logged in" + username)
